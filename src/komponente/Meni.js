a68712a580b1b6556fc67035d4fe7a8a1b504608
vukDjedovic <vukdjedovic98@gmail.com>
2022-06-21 22:53:11 +0200

    Početna strana aplikacije, navigacioni meni, css, rutiranje

---
+++ src/komponente/Meni.js
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom';
+
+function Meni(){
+    return (
+        <div>
+            <ul>
+                <Link to={"/"}><li>Početna</li></Link>
+            </ul>
+        </div>
+    )
+}
+
+export default Meni;
---
+++ src/page/Pocetna.js
@@ -0,0 +1,11 @@
+import Meni from "../komponente/Meni";
+
+function Pocetna(){
+    return (
+        <div>
+            <Meni />
+        </div>
+    )
+}
+
+export default Pocetna;
